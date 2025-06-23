68fd43e2a09961ea46b0006bfe126f9cdbd92fac
Ajay Pal <ajaypal@Lakshays-MacBook-Pro.local>
2025-06-23 17:38:49 +0530

    minnor version fix

--- dist/components/CCTVVideoBar/index.d.ts
+++ dist/components/CCTVVideoBar/index.d.ts
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ProgressScaleProp } from '../types/types';
-declare const ProgressScaleBar: React.NamedExoticComponent<ProgressScaleProp>;
-export { ProgressScaleBar };
-declare const _default: React.NamedExoticComponent<ProgressScaleProp>;
-export default _default;
+import './index.css';
+declare const ProgressScale: React.NamedExoticComponent<ProgressScaleProp>;
+export { ProgressScale };
